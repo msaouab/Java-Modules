25786801e7fa7c4a7602d8af79fa8fc7431676c3
msaouab <msaouab@student.1337.ma>
2024-03-04 16:23:56 +0100

    feat: javaModule/ex03 done

--- Java-Module-00/ex03/Program.java
+++ Java-Module-00/ex03/Program.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -5,21 +7,90 @@ import java.util.Scanner;
  */
 public class Program {
 
+	/**
+	 * printError
+	 * 
+	 * @param message String
+	 * @return void
+	 */
+	static void printError(String message) {
+		System.err.println(message);
+		System.exit(-1);
+	}
+
+	/**
+	 * printWeekGrades
+	 * 
+	 * @param weekGrades
+	 * @param currentWeek
+	 * @return void
+	 */
+	static void printWeekGrades(Map<Integer, Integer> weekGrades, int currentWeek) {
+		for (int i = 1; i <= currentWeek; i++) {
+			System.out.print("\tWeek " + i + " ");
+			if (weekGrades.containsKey(i)) {
+				int minScore = weekGrades.get(i);
+				for (int j = 0; j < minScore; j++) {
+					System.out.print("=");
+				}
+				System.out.println(">");
+			}
+		}
+	}
+
+	/**
+	 * Main entry point
+	 * 
+	 * @param args
+	 * @return void
+	 */
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		try {
-			int maxWeeks = 18 * 2;
-			while (maxWeeks-- > 0) {
-				System.out.print("-> ");
+		Map<Integer, Integer> weekGrades = new HashMap<>();
+		final int MAX_WEEKS = 18;
 
-				int number = scanner.nextInt();
-				if (number == 42) {
+		try {
+			int currentWeek = 0;
+			int week = 1;
+			System.out.print("-> ");
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine().trim();
+
+				if (line.equals("42")) {
 					break;
 				}
-            }
-			System.out.println("End of the program: " + maxWeeks);
+
+				System.out.print("-> ");
+				String[] gradesScore = null;
+
+				if (line.startsWith("Week")) {
+					week = Integer.parseInt(line.split("\\s+")[1]);
+					if (week != currentWeek + 1)
+						printError("IllegalArgument");
+					currentWeek = week;
+				} else {
+					gradesScore = line.split("\\s+");
+
+					if (gradesScore.length != 5)
+						printError("IllegalArgument: Exactly 5 tests are required for each week.");
+					int minGrade = Integer.MAX_VALUE;
+					for (String g : gradesScore) {
+						int grade = Integer.parseInt(g);
+						if (grade < 1 || grade > 9)
+						printError("IllegalArgument: Each test must be graded between 1 and 9.");
+						minGrade = Math.min(minGrade, grade);
+					}
+					weekGrades.put(currentWeek, minGrade);
+				}
+				if (weekGrades.size() == MAX_WEEKS)
+					break;
+			}
+
+			printWeekGrades(weekGrades, currentWeek);
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
 		} finally {
 			scanner.close();
 		}
