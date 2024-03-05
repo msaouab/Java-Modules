03b843f3c5b91cef3ea2d3a667654e3ba4b6f06f
msaouab <msaouab@student.1337.ma>
2024-03-05 16:34:55 +0100

    feat: javaModule/ex00 done

--- Java-Module-00/ex00/Program.java
+++ Java-Module-00/ex00/Program.java
@@ -3,26 +3,14 @@ public class Program {
 	public static void main(String[] args) {
 		int sixDigitNumber = 123456;
 
-		// check if the number is int
-		if (sixDigitNumber != (int)sixDigitNumber) {
-			System.out.println("The variable: `sixDigitNumber` is not a int number");
-			return;
-		}
-		// convert the number to a string
-		String numbeString = String.valueOf(sixDigitNumber);
-
-		// check if the number is a six digit number
-		if (numbeString.length() != 6) {
-			System.out.println("The number: `" + numbeString + "` is not a six digit number");
-			return;
-		}
-		// calculate the sum of the digits
 		int sum = 0;
-		while (sixDigitNumber > 0) {
-			sum += sixDigitNumber % 10;
-			sixDigitNumber /= 10;
-		}
-
-		System.out.println("The sum of the digits is: " + sum);
+		sum += sixDigitNumber / 100000;
+		sum += (sixDigitNumber / 10000) % 10;
+		sum += (sixDigitNumber / 1000) % 10;
+		sum += (sixDigitNumber / 100) % 10;
+		sum += (sixDigitNumber / 10) % 10;
+		sum += sixDigitNumber % 10;
+		
+		System.out.println("-> " + sum);
 	}
 }
